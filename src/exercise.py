9a7f309eba8d688974a537058fe1c4e6bda78ece
Jaime Santos <50669048+JaimePSantos@users.noreply.github.com>
2020-10-21 14:24:24 +0100

    ex

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
-    #write your code below this line
+    print("How many times?")
+    times = int(input())
+    i =0
+    while(i<times):
+        print_text()
+        i+=1
+
+def print_text():
+    print("In a hole in the ground there lived a method")
 
 if __name__ == '__main__':
     main()
